c9697535d2ab743b72f2dfe76888cc1535f06b23
Chris McIntosh <httpstack@gmail.com>
2025-07-26 09:11:30 -0700

    Implement resume page with download functionality and lightbox viewer; refactor routing for new methods

--- HttpStack/App/Controllers/Routes/PublicController.php
+++ HttpStack/App/Controllers/Routes/PublicController.php
@@ -22,8 +22,9 @@ class PublicController{
             $res->send();
         }   
     }
-    public function about($req, $res,$container,$matches){
-        $res->setContentType("text/html")->setBody("About Page");
+    public function resume($req, $res,$container,$matches){
+        $v = $container->make("view", "public/resume");
+        $v->render();
         if(!$res->sent){
             $res->send();
         }
@@ -35,9 +36,8 @@ class PublicController{
             $res->send();
         }
     }
-
-    public function resume($req, $res,$container,$matches){
-        $res->setContentType("text/html")->setBody("Resume Page");
+        public function about($req, $res,$container,$matches){
+        $res->setContentType("text/html")->setBody("about Page");
         if(!$res->sent){
             $res->send();
         }
